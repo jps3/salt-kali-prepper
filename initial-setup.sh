eedacd0e89e08c1135438a56906f31f9af2a9030
Jason Stanford <jps3@lehigh.edu>
2019-05-22 21:22:59 -0400

    Minor whitespace changes or fixes

--- initial-setup.sh
+++ initial-setup.sh
@@ -11,8 +11,8 @@
 wget -O - https://repo.saltstack.com/apt/debian/9/amd64/latest/SALTSTACK-GPG-KEY.pub \
 | sudo apt-key add -
 
-echo 'deb http://repo.saltstack.com/apt/debian/9/amd64/latest stretch main'          \
-| tee -a /etc/apt/sources.list.d/saltstack.list
+echo 'deb http://repo.saltstack.com/apt/debian/9/amd64/latest stretch main' \
+| tee /etc/apt/sources.list.d/saltstack.list
 
 apt-get update
 
