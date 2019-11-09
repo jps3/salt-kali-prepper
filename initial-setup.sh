c6bff2b257976fee97d91f2894eee91cf1f96f46
Jason Stanford <jps3@lehigh.edu>
2019-11-09 17:30:26 -0500

    Refactored initial-setup.sh script
      * Added lib_color_logging.sh (borrowed from rpi-img-build-box repo,
        renamed, and updated to conform to shellcheck rules).
      * Updated output and formatting for easier reading and visual
        scanning.
      * Added check for major Kali releases (2018, 2019) to use appropriate
        SaltStack apt repository.

--- initial-setup.sh
+++ initial-setup.sh
@@ -8,59 +8,106 @@
 # 
 # ----------------------------------------------------------------------
 
-echo
-echo "************************************************************"
-echo " 1. Updating apt lists ..."
-echo " 2. Installing saltstack's salt-minion ..."
-echo " 3. Installing python git ..."
-echo "************************************************************"
+declare -a pkg_list
+pkg_list=( "salt-minion" "python3-git" )
 
-apt-get update
+# shellcheck source=lib_color_logging.sh
+source lib_color_logging.sh
 
+
+# ----------------------------------------------------------------------
+
+print_header "BEGIN"
+
+log "Sourcing os-release"
+# shellcheck disable=SC1091
+source /etc/os-release
+
+info "PRETTY_NAME" "${PRETTY_NAME}"
+info "VERSION"     "${VERSION}"
+
+
+# ----------------------------------------------------------------------
+
+print_header "Configure APT"
+
+if [[ "${ID_LIKE}" == "debian" ]]; then
+  case "${VERSION%%\.*}" in
+    2019) APT_KEY_URL="https://repo.saltstack.com/py3/debian/10/amd64/latest/SALTSTACK-GPG-KEY.pub";
+          APT_SOURCE="deb http://repo.saltstack.com/py3/debian/10/amd64/latest buster main"
+          ;;
+    2018) APT_KEY_URL="https://repo.saltstack.com/py3/debian/9/amd64/latest/SALTSTACK-GPG-KEY.pub";
+          APT_SOURCE="deb http://repo.saltstack.com/py3/debian/9/amd64/latest stretch main"
+          ;;
+       *) echo "** NOT SUPPORTED **"
+          exit 1
+          ;;
+  esac
+fi
+
+info "APT_KEY_URL" "${APT_KEY_URL}"
+log "Adding apt key for saltstack repo ..."
+wget --quiet -O - "${APT_KEY_URL}" | apt-key add -
+
+info "APT_SOURCE" "${APT_SOURCE}"
+log "Adding apt source for saltstack repo ..."
+echo "${APT_SOURCE}" | tee "/etc/apt/sources.list.d/saltstack.list"
+
+
+# ----------------------------------------------------------------------
+
+print_header "Install SaltStack"
+
+log "Updating APT ..."
+apt-get update -d -qq
+
+log "Installing packages ..."
 apt-get \
   -o Dpkg::Options::="--force-confdef" \
   -o Dpkg::Options::="--force-confold" \
   install \
-  -y \
-  salt-minion \
-  python-git \
-  python3-git
+  -y -qq \
+  "${pkg_list[@]}"
 
-echo
-echo "************************************************************"
-echo " 4. Verifying salt-minion installed:"
-echo "************************************************************"
-dpkg-query --show --showformat '${Status} ${Package} ${Version} ${Architecture}' salt-minion
 
-echo
-echo
-echo "************************************************************"
-echo " 5. Disabling salt-minion service "
-echo "    (only used on-demand with --local)"
-echo "************************************************************"
-set -x
+# ----------------------------------------------------------------------
+
+print_header "Verifying Installation"
+
+for pkg in "${pkg_list[@]}"; do
+  log "Checking ${pkg} ..."
+  dpkg-query --show --showformat '${Status} ${Package} ${Version} ${Architecture}\n' "${pkg}"
+done
+
+
+# ----------------------------------------------------------------------
+
+print_header "Configure salt-minion"
+log "Disabling salt-minion service (only --local used)"
 update-rc.d salt-minion disable
-set +x
 
-echo
-echo "************************************************************"
-echo " 6. Copying files to /etc/salt, /srv/salt, /srv/pillar"
-echo "************************************************************"
-set -x
+
+# ----------------------------------------------------------------------
+
+print_header "Copy SaltStack support files to /srv/"
+
+log "Copying *.conf files to /etc/salt/minion.d ..."
 cp -R etc/salt/minion.d/*.conf /etc/salt/minion.d/
+log "Copying pillar files to /srv/ ..."
 cp -R srv/pillar /srv/
-cp -R srv/salt   /srv/
-set +x
+log "Copying salt files to /srv/ ..."
+cp -R srv/salt /srv/
 
-echo
-echo "************************************************************"
-echo
-echo " Try applying the masterless (local) salt states:"
-echo " $ salt-call --local state.apply"
-echo
-echo " Or, with debugging output:"
-echo " $ salt-call --log-level=debug --local state.apply"
-echo
-echo "************************************************************"
 
+# ----------------------------------------------------------------------
+
+print_header "Installation Complete"
+
+log " Try applying the masterless (local) salt states:"
+log " $ salt-call --local state.apply"
+log ""
+log " Or, with debugging output:"
+log " $ salt-call --log-level=debug --local state.apply"
+
+print_header "END."
 exit 0
